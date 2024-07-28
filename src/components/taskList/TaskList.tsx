17960609406bdd16b12dde0d28ccd2b8fd944fb5
Majedah <122641068+majowl77@users.noreply.github.com>
2024-07-28 11:44:01 +0300

    add new task and delete it

--- src/components/taskList/TaskList.tsx
+++ src/components/taskList/TaskList.tsx
@@ -4,18 +4,29 @@ type TaskList = {
   taskItem: string;
 };
 export default function TaskList() {
-  const [taskList, setTaskList] = useState<TaskList[]>();
+  let [taskList, setTaskList] = useState<TaskList[]>([]);
   const [taskItem, setTaskItem] = useState("");
 
-  function addNewTask(newTask: string) {
-    setTaskList([{ taskItem: newTask }]);
+  function addNewTask(e: React.FormEvent<HTMLFormElement>) {
+    setTaskList((prevTaskList) => [...prevTaskList, { taskItem: taskItem }]);
     setTaskItem("");
+    e.preventDefault();
+  }
+
+  function deleteTask(taskItem: string) {
+    taskList = taskList.filter((task) => task.taskItem != taskItem);
+    if (taskList) {
+      console.log("task was delete it");
+    }
+    setTaskList((prevTaskList) =>
+      prevTaskList.filter((task) => task.taskItem !== taskItem)
+    );
   }
 
   return (
     <section>
       <h6> Your To do lsit</h6>
-      <form>
+      <form onSubmit={addNewTask}>
         <label> Enter</label>
         <input
           type="text"
@@ -24,13 +35,18 @@ export default function TaskList() {
           value={taskItem}
           onChange={(e) => setTaskItem(e.target.value)}
         />
-        <button onClick={() => addNewTask} > + </button>
+        <button type="submit"> + </button>
       </form>
 
       <div>
         here the list of your task :
         {taskList?.length &&
-          taskList.map((task) => <h6 key={task.taskItem}> {task.taskItem}</h6>)}
+          taskList.map((task) => (
+            <div key={task.taskItem}>
+              <h6> {task.taskItem}</h6>
+              <button onClick={() => deleteTask(task.taskItem)}>Delete</button>
+            </div>
+          ))}
       </div>
     </section>
   );
